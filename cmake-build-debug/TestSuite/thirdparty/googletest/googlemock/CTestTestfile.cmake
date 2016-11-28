0d1ff847f7b4931e588f98e869fe22bb8f7a872d
Llikdowk <guillermofm7@gmail.com>
2016-11-28 17:50:33 +0100

    removed auto generated bin dir

--- cmake-build-debug/TestSuite/thirdparty/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/WORKSPACE/C++/Rasterizer/TestSuite/thirdparty/googletest/googlemock
-# Build directory: D:/WORKSPACE/C++/Rasterizer/cmake-build-debug/TestSuite/thirdparty/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- cmake-build-debug/TestSuite/thirdparty/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/WORKSPACE/C++/Rasterizer/TestSuite/thirdparty/googletest/googletest
-# Build directory: D:/WORKSPACE/C++/Rasterizer/cmake-build-debug/TestSuite/thirdparty/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
